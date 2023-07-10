4f50796d2047bf51e466bcd13aa2e7b6487e1e7d
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-07-10 17:36:19 +0200

    7-Validaciones y optimizaciones

--- 07-todo-app/src/todos/use-cases/render-todo.js
+++ 07-todo-app/src/todos/use-cases/render-todo.js
@@ -1,14 +1,20 @@
 import { Todo } from '../models/todo.model';
 import { createTodoHTML } from './';
 
+let element;
+
 /**
  * Dado el id del elemento se renderiza ahí
  * @param {String} elementId
  * @param {Todo} todos
  */
 export const renderTodos = (elementId, todos = []) => {
-  // TODO: referencia. La idea es evitar que se salte al DOM siempre, manteniendo la misma referencia y si lo tenemos, no volverlo a buscar.
-  const element = document.querySelector(elementId);
+  // Referencia. La idea es evitar que se salte al DOM siempre, manteniendo la misma referencia y si lo tenemos, no volverlo a buscar.
+  if (!element) element = document.querySelector(elementId);
+
+  if (!element) throw new Error(`Element ${elementId} not found`);
+
+  element.innerHTML = '';
 
   todos.forEach((todo) => {
     element.append(createTodoHTML(todo));
