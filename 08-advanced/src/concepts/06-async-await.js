9d2c89557483cad4325ce02ad0eb8a3270621323
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-07-13 17:09:21 +0200

    8-Errores en Async-Await

--- 08-advanced/src/concepts/06-async-await.js
+++ 08-advanced/src/concepts/06-async-await.js
@@ -11,26 +11,34 @@ export const asyncAwaitComponent = async (element) => {
   const id1 = '5d86371f2343e37870b91ef1';
   const id2 = '5d86371f1efebc31def272e2';
 
-  // Con async-await podemos tomar los valores de las funciones asíncronas
-  // como si fueran funciones tradicionales síncronas.
-  //
-  // Sin await, hero1 es una promesa.
-  // Con await, hero1 es el objeto ya producto resuelto de mi función. Se espera a que termine!
-  //
-  // Con esto ya no existen los callback hell.
-  const { name: name1 } = await findHero(id1);
-  const { name: name2 } = await findHero(id2);
+  // Las excepciones de async-await se tratan en un try-catch
+  try {
+    // Con async-await podemos tomar los valores de las funciones asíncronas
+    // como si fueran funciones tradicionales síncronas.
+    //
+    // Sin await, hero1 es una promesa.
+    // Con await, hero1 es el objeto ya producto resuelto de mi función. Se espera a que termine!
+    //
+    // Con esto ya no existen los callback hell.
+    const { name: name1 } = await findHero(id1);
+    const { name: name2 } = await findHero(id2);
 
-  // Problema: Si la búsqueda del primer heroe se demora segundo y medio y la del segundo heroe
-  //    se demora otro segundo y medio, entonces esta línea va a empezar a ejecutarse 3sg después.
-  // Este problema no tenemos por qué tenerlo si una promesa no depende de la otra.
-  // Es obligatorio seguir este camino si una promesa depende del resultado de la anterior.
-  element.innerHTML = `${name1} / ${name2}`;
+    // Problema: Si la búsqueda del primer heroe se demora segundo y medio y la del segundo heroe
+    //    se demora otro segundo y medio, entonces esta línea va a empezar a ejecutarse 3sg después.
+    // Este problema no tenemos por qué tenerlo si una promesa no depende de la otra.
+    // Es obligatorio seguir este camino si una promesa depende del resultado de la anterior.
+    element.innerHTML = `${name1} / ${name2}`;
+  } catch (error) {
+    element.innerHTML = error;
+  }
 };
 
 const findHero = async (id) => {
   const hero = heroes.find((hero) => hero.id === id);
+  // Si queremos que la promesa se rechace se hace con un throw, no con return.
+  // La función llamadora estará en un try-catch
   if (!hero) throw `hero not found`;
 
+  // Recordar que un return SIEMPRE es existoso!!
   return hero;
 };
