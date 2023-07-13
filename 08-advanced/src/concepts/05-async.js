c3a2ab07bfa12c07e2a38b9df2d0c106fcf04a92
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-07-13 16:37:58 +0200

    8-Manejo de errores en funciones asincronas

--- 08-advanced/src/concepts/05-async.js
+++ 08-advanced/src/concepts/05-async.js
@@ -5,10 +5,18 @@ import { heroes } from '../data/heroes';
  * @param {HTMLDivElement} element
  */
 export const asyncComponent = (element) => {
-  const id1 = '5d86371fd55e2e2a30fe1ccb';
+  const id1 = '5d86371fd55e2e2a30fe1ccbx';
+
+  console.log('Inicio de Componente');
+
   findHero(id1)
     .then((name) => (element.innerHTML = name))
+    // .then(console.log)
     .catch((error) => (element.innerHTML = error));
+
+  findHeroWithTryCatch(id1).then(console.log);
+
+  console.log('Fin del Componente');
 };
 
 // Con la palabra reservada async cambia por completo como funciona la función.
@@ -21,5 +29,25 @@ export const asyncComponent = (element) => {
  */
 const findHero = async (id) => {
   const hero = heroes.find((hero) => hero.id === id);
-  return hero?.name;
+
+  // Manejo del error correcto. Si no hemos obtenido ningún heroe no queremos hacer return.
+  // Se llama al reject.
+  if (!hero) throw `Hero with id ${id} not found`;
+
+  // Un return es que todo salió bien, siempre es llamar al resolve.
+  return hero.name;
+};
+
+// Mucho cuidado con esto.
+// Como tenemos un try - catch, si falla se devuelve lo que tengamos en el catch.
+// Y como lo que tenemos en el catch es un return lo TRATA COMO RESOLVE, un exito.
+const findHeroWithTryCatch = async (id) => {
+  try {
+    const hero = heroes.find((hero) => hero.id === id);
+    if (!hero) throw `Hero with id ${id} not found`;
+    return hero.name;
+  } catch (error) {
+    // RECORDAR: UN RETURN ES UN EXITO!
+    return 'hola mundo';
+  }
 };
