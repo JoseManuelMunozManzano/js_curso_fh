466885861a5ee74ff0a4afffff0a44de71dbe467
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2023-07-14 17:47:27 +0200

    9-Fetch API - Petición http get

--- 09-http-app/src/breakingbad/breakingbad-app.js
+++ 09-http-app/src/breakingbad/breakingbad-app.js
@@ -2,10 +2,27 @@
 // Aquí vamos a renderizar nuestra aplicación.
 
 /**
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async () => {
+  // Esta es la respuesta completa, con el body, los headers, el status...
+  const res = await fetch('https://api.breakingbadquotes.xyz/v1/quotes');
+
+  // Este es el body de la petición.
+  const data = await res.json();
+  console.log(data);
+
+  return data;
+};
+
+/**
  *
  * @param {HTMLDivElement} element
  */
-export const BreakingBadApp = (element) => {
+export const BreakingBadApp = async (element) => {
   document.querySelector('#app-title').innerHTML = 'Breaking Bad App';
   element.innerHTML = 'Loading...';
+
+  const quote = await fetchQuote();
+  element.innerHTML = 'Tenemos data!!';
 };
